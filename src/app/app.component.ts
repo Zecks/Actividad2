172fb6437b42ca5944ceb310b6d756d160f9346a
Zecks <erodrigo46@gmail.com>
2025-03-01 11:07:53 -0500

    Actualizacion de la pagina de inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CabeceraComponent } from './components/cabecera/cabecera.component';
+import { NavegacionComponent } from './components/navegacion/navegacion.component';
+import { PieComponent } from './components/pie/pie.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CabeceraComponent, NavegacionComponent, PieComponent, InicioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
